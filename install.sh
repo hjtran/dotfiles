b1eb54736bebef112252bb4d298dc2e8babda5d9
Joey Tran <joey.tran@schrodinger.com>
2017-10-22 22:05:09 -0400

    update with new plugins and removed solarized

--- install.sh
+++ install.sh
@@ -36,11 +36,21 @@ case $yn in
 		echo 'installing syntastic...'
 		cd ~/.vim/bundle
 		git clone https://github.com/scrooloose/syntastic.git
-		echo 'done~'
+		echo 'done!'
 
-		echo 'installing solarized...'
+		echo 'installing fzf...'
 		cd ~/.vim/bundle
-		git clone git://github.com/altercation/vim-colors-solarized.git
+		git clone https://github.com/junegunn/fzf.vim
+		echo 'done!'
+
+		echo 'installing fugitive...'
+		cd ~/.vim/bundle
+		git clone https://github.com/tpope/vim-fugitive
+		echo 'done!'
+
+		echo 'installing airline...'
+		cd ~/.vim/bundle
+		git clone https://github.com/vim-airline/vim-airline
 		echo 'done!';;
 
 	* ) exit;;
