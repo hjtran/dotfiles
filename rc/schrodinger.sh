b3dd96ab61814031bd25884463c6d9e42d502179
Joey Tran <joey.tran@schrodinger.com>
2017-11-14 08:07:49 -0500

    Integrate testdinger with fzf
    Now running testdinger with -f uses FZF to find a test file, -d searches
    for directories, -r uses the last test args, and no flags will run just
    the last failing tests

--- rc/schrodinger.sh
+++ rc/schrodinger.sh
@@ -1,28 +1,6 @@
 ### Bash settings for schrodinger development
 
-############## Functions
-rbt () {
-    ## Function for saving rbt ids for branches so I don't have to type in
-    ## rbt -r and remember what number the review was.
-    repo=$(basename `git rev-parse --show-toplevel`)
-    branch=$(git branch | grep \* | cut -d ' ' -f2)
-    rbt_id=$(grep "$repo:$branch" ~/.rbtids | cut -d":" -f3)
-    if [ -z "$rbt_id" ]; then
-        rbt_id=$(/usr/local/bin/rbt post --open --bugs-closed $branch --summary $branch: | tee /dev/tty | egrep -o '[0-9]+' | head -1)
-        echo -e "$repo:$branch:$rbt_id\n" >> ~/.rbtids
-    else
-        /usr/local/bin/rbt post -r $rbt_id
-    fi
-}
-
-testdinger () {
-    ## Function to run unit tests
-    build_modules
-    cd $SCHRODINGER/mmshare*/python/test
-    make unittest TEST_ARGS="$1"
-    cd -
-}
-
+############## Environment Setup Functions
 set_schrodinger_v () {
     SCHRODINGER=/scr/jtran/builds/$1/build
     export SCHRODINGER
@@ -35,6 +13,15 @@ set_schrodinger_v () {
 schrodinger_v () {
     echo '$SCHRODINGER: '$SCHRODINGER
     echo '$SCHRODINGER_SRC: '$SCHRODINGER_SRC
+}
+
+schropy3 () {
+    export SCHRODINGER_PYTHON3=true
+    export SCHRODINGER=/scr/jtran/builds/2018-1py3/build
+}
+schropy2 () {
+    unset SCHRODINGER_PYTHON3
+    export SCHRODINGER=/scr/jtran/builds/2018-1/build
 }
 
 ############## Environment Setup
@@ -53,3 +40,52 @@ dtgui=$dt'/application/msv/gui'
 # Load aliases
 source $dotfiles'/alias/schrodinger.sh'
 
+
+############## General Functions
+testdinger () {
+    ## Function to run unit tests
+    start_dir=$PWD
+    ## Default, run failed tests from last run
+    test_args=$failed_tests
+    build_modules
+    while getopts "fdrn:" opt; do
+      case $opt in
+        f)
+            cd $SCHRODINGER_SRC/mmshare/python/test
+            test_args=$(find . -name "*.py" | sed 's/\.\///g' | fzf)
+          ;;
+        d)
+            cd $SCHRODINGER_SRC/mmshare/python/test
+            test_args=$(find . -type d | sed 's/\.\///g' | fzf)
+          ;;
+        n)
+            test_args=$OPTARG
+          ;;
+        r)
+            test_args=$old_test_args
+        ;;
+        \?)
+          echo "Invalid option: -$OPTARG" >&2
+          ;;
+      esac
+    done
+    cd $SCHRODINGER/mmshare*/python/test
+    failed_tests=$(script -q /dev/null make unittest TEST_ARGS="$(echo $test_args)" | tee /dev/tty | egrep -o '[A-Za-z][^ ]+::[^ ]+::[^ ]+')
+    old_test_args=$test_args
+    cd $start_dir
+    unset OPTIND
+}
+
+rbt () {
+    ## Function for saving rbt ids for branches so I don't have to type in
+    ## rbt -r and remember what number the review was.
+    repo=$(basename `git rev-parse --show-toplevel`)
+    branch=$(git branch | grep \* | cut -d ' ' -f2)
+    rbt_id=$(grep "$repo:$branch" ~/.rbtids | cut -d":" -f3)
+    if [ -z "$rbt_id" ]; then
+        rbt_id=$(/usr/local/bin/rbt post --open --bugs-closed $branch --summary $branch: | tee /dev/tty | egrep -o '[0-9]+' | head -1)
+        echo -e "$repo:$branch:$rbt_id\n" >> ~/.rbtids
+    else
+        /usr/local/bin/rbt post -r $rbt_id
+    fi
+}
